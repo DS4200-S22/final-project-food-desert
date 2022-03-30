ac2793a9b7202310175ceab7823de06ea0a6cea9
angelinahan <han.ang@northeastern.edu>
2022-03-30 19:21:34 -0400

    add map with topojson

--- js/visualization.js
+++ js/visualization.js
@@ -1,4 +1,4 @@
-//This is filler -- delete it and start coding your visualization tool here
+// //This is filler -- delete it and start coding your visualization tool here
 // d3.select("#vis-container")
 //   .append("text")
 //   .attr("x", 20)
@@ -11,6 +11,9 @@
 //   let projection = d3.geoAlbersUsa();
 //   projection.fitSize([width, height], bb);
 //   let geoGenerator = d3.geoPath().projection(projection);
+
+//   let counties = topojson.feature(us, us.objects.counties).features;
+//   console.log(counties);
 
 //   let svg = d3
 //     .select("#vis-container")
@@ -28,9 +31,66 @@
 //     .attr("stroke", "#000");
 // });
 
-projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
-path = d3.geoPath();
-us = FileAttachment("counties-albers-10m.json").json();
+//https://d3js.org/us-10m.v1.json
 
-topojson = require("topojson-client@3");
-d3 = require("d3-geo@2");
+d3.json("https://d3js.org/us-10m.v1.json").then(function (us) {
+  // if (error) {
+  //   console.log("Error!");
+  //   console.log(error);
+  // }
+
+  console.log(us);
+
+  let width = 1000,
+    height = 800;
+
+  let svg = d3
+    .select("#vis-container")
+    .append("svg")
+    .style("width", width)
+    .style("height", height);
+
+  let path = d3.geoPath();
+
+  svg
+    .append("g")
+    .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter()
+    .append("path")
+    .attr("d", path);
+
+  svg
+    .append("path")
+    .attr("class", "state-borders")
+    .attr(
+      "d",
+      path(
+        topojson.mesh(us, us.objects.states, function (a, b) {
+          return a !== b;
+        })
+      )
+    );
+
+  let zoom = d3.behavior.zoom().on("zoom", function () {
+    g.attr(
+      "transform",
+      "translate(" +
+        d3.event.translate.join(",") +
+        ")scale(" +
+        d3.event.scale +
+        ")"
+    );
+    g.selectAll("path").attr("d", path.projection(projection));
+  });
+
+  svg.call(zoom);
+});
+
+// projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
+// path = d3.geoPath().projection(projection);
+// us = FileAttachment("counties-albers-10m.json").json();
+
+// topojson = require("topojson-client@3");
+// d3 = require("d3-geo@2");
