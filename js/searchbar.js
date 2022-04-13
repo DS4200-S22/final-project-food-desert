bbbbec8bd854a69b405a9ab259f18916f88c59e0
angelinahan <han.ang@northeastern.edu>
2022-04-13 11:43:03 -0400

    linked search bar and map to use the same selected counties

--- js/searchbar.js
+++ js/searchbar.js
@@ -6,23 +6,76 @@ const populateSearchBar = (data) => {
   const counties3 = d3.select("#counties3");
   data.forEach((d) => {
     const countyName = `${d.County}, ${d.State}`;
-    counties1.append("option").attr("value", countyName);
-    counties2.append("option").attr("value", countyName);
-    counties3.append("option").attr("value", countyName);
+    const id = d.id;
+    counties1.append("option").attr("value", countyName).attr("id", id);
+    counties2.append("option").attr("value", countyName).attr("id", id);
+    counties3.append("option").attr("value", countyName).attr("id", id);
   });
+};
+
+const updateSearchBar = () => {
+  const searchbar1 = document.getElementById("searchbar1");
+  const searchbar2 = document.getElementById("searchbar2");
+  const searchbar3 = document.getElementById("searchbar3");
+  if (counties[0] !== undefined) {
+    const county1Id = counties[0];
+    searchbar1.value = id_to_name.get(county1Id);
+  } else {
+    searchbar1.value = "";
+  }
+  if (counties[1] !== undefined) {
+    const county2Id = counties[1];
+    searchbar2.value = id_to_name.get(county2Id);
+  } else {
+    searchbar2.value = "";
+  }
+  if (counties[2] !== undefined) {
+    const county3Id = counties[2];
+    searchbar3.value = id_to_name.get(county3Id);
+  } else {
+    searchbar3.value = "";
+  }
+};
+
+const clearSelection = (selection) => {
+  const prevValue = selection.value;
+  selection.value = "";
+  const index = counties.indexOf(name_to_id.get(prevValue));
+  if (index !== -1) {
+    counties.splice(index, 1);
+    showHideOutline(prevValue);
+  }
 };
 
 //https://stackoverflow.com/questions/30022728/perform-action-when-clicking-html5-datalist-option
 //add to this later when we connect county selection to the visualizations
-function onInput() {
-  let value = document.getElementById("searchbar1").value; //the value current in the first search bar
-  let opts = document.getElementById("counties1").childNodes; //the datalist options
-  for (var i = 0; i < opts.length; i++) {
-    if (opts[i].value === value) {
-      // An item was selected from the list!
-      // yourCallbackHere()
-      //   alert(opts[i].value);
-      break;
-    }
+function onInput(selectedLocation) {
+  if (counties.length >= 3) {
+    alert("Can't select more than 3");
+  } else if (counties.indexOf(name_to_id.get(selectedLocation)) === -1) {
+    const id = name_to_id.get(selectedLocation);
+    counties.push(id);
+
+    //show county outline on map
+    showHideOutline(selectedLocation);
+
+    update_bar();
   }
 }
+
+const showHideOutline = (location) => {
+  const countyState = location.split(", ");
+  const county = countyState[0];
+  const state = countyState[1];
+  const stateName = state_code_to_name.get(state);
+  const elementId = `${county}${stateName}`;
+  console.log(elementId);
+  let countyElem = document.getElementById(elementId);
+  if (countyElem !== undefined) {
+    if (countyElem.classList.contains("county-selected")) {
+      countyElem.classList.remove("county-selected");
+    } else {
+      countyElem.classList.add("county-selected");
+    }
+  }
+};
--- js/visualization.js
+++ js/visualization.js
@@ -1,6 +1,6 @@
 let width_map = 960;
 let height_map = 750;
-const margin_map = {left:50, right:50, bottom:50, top:50};
+const margin_map = { left: 50, right: 50, bottom: 50, top: 50 };
 let color_domain = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 let ext_color_domain = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
 let legend_labels = [
@@ -20,6 +20,13 @@ let color = d3.scaleThreshold().domain(color_domain).range(d3.schemeBlues[9]);
 
 // build map of county names to PCT_LACCESS_POP15
 let pct_data = new Map();
+// build map of county ids to their names (county + state name)
+let id_to_name = new Map();
+// build map of county names (county + state name) to their ids
+let name_to_id = new Map();
+// build map of county names from Albers to county ids in our own dataset
+let FIPS = new Map();
+let state_code_to_name = new Map();
 
 // create functions for tooltip to show name of moused-over county
 let mouseover_map;
@@ -34,88 +41,112 @@ let svg = d3
   .attr("width", width_map + margin_map.left + margin_map.right)
   .attr("height", height_map + margin_map.top + margin_map.bottom);
 
-let map = svg.append("g")
-    .attr('class', 'map')
-    .attr('transform', 'translate('+margin_map.left+','+margin_map.top+')')
-    .attr('width', width_map + margin_map.left + margin_map.right)
-    .attr('height', height_map + margin_map.top + margin_map.bottom)
+let map = svg
+  .append("g")
+  .attr("class", "map")
+  .attr(
+    "transform",
+    "translate(" + margin_map.left + "," + margin_map.top + ")"
+  )
+  .attr("width", width_map + margin_map.left + margin_map.right)
+  .attr("height", height_map + margin_map.top + margin_map.bottom);
 
 let path = d3.geoPath();
 
-const zoom = d3.zoom()
-      .scaleExtent([1, 8])
-      .on("zoom", zoomed);
-      
+const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+
 // load counties geojson and our food desert data
 Promise.all([
   d3.json("data/counties-albers-10m.json"),
   d3.csv("data/finaldata.csv", function (d) {
     pct_data.set(d.County, d.PCT_LACCESS_POP15);
+    id_to_name.set(d.FIPS, `${d.County}, ${d.State}`);
+    name_to_id.set(`${d.County}, ${d.State}`, d.FIPS);
+    FIPS.set(`${d.County}${d.FIPS.substring(0, 2)}`, d.FIPS);
+    return d;
+  }),
+  d3.tsv("data/us-state-names.tsv", function (state) {
+    state_code_to_name.set(state.code, state.name);
   }),
 ]).then((values) => ready(values[0]));
 
 function ready(us) {
-  
   /* 
     Tooltip  
   */
 
-  const tooltip_map = d3.select("#vis-container") 
-  .append("div") 
-    .attr('id', "tooltip-map") 
-    .style("opacity", 0) 
-    .attr("class", "tooltip"); 
+  const tooltip_map = d3
+    .select("#vis-container")
+    .append("div")
+    .attr("id", "tooltip-map")
+    .style("opacity", 0)
+    .attr("class", "tooltip");
+
+  const topojson_states = topojson.feature(us, us.objects.states).features;
 
   // when moused over, tooltip shows name of data and the score
-  mouseover_map = function(event, d) {
-    topojson_states = topojson.feature(us, us.objects.states).features
-    current_state = topojson_states.filter(function(s) { return d.id.substring(0,2) == s.id; });
-    tooltip_map.html(d.properties.name + ", " + current_state[0].properties.name) 
-    .style("opacity", 1); 
-  }
+  mouseover_map = function (event, d) {
+    current_state = topojson_states.filter(function (s) {
+      return d.id.substring(0, 2) == s.id;
+    });
+    tooltip_map
+      .html(d.properties.name + ", " + current_state[0].properties.name)
+      .style("opacity", 1);
+  };
 
-  // when mouse moves, the tooltip will show where mouse is 
-  mousemove_map = function(event, d) {
-    tooltip_map.style("left", (event.pageX + 5)+"px") 
-    .style("top", (event.pageY - 25) +"px"); 
-  }
+  // when mouse moves, the tooltip will show where mouse is
+  mousemove_map = function (event, d) {
+    tooltip_map
+      .style("left", event.pageX + 5 + "px")
+      .style("top", event.pageY - 25 + "px");
+  };
 
   // when moused out, tooltip disappears
-  mouseleave_map = function(event, d) { 
-    tooltip_map.style("opacity", 0); 
-  }
+  mouseleave_map = function (event, d) {
+    tooltip_map.style("opacity", 0);
+  };
 
   /*
     Map
   */
 
-  map.append("g")
+  map
+    .append("g")
     .attr("id", "counties")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .enter()
     .append("path")
     // draw each county
-      .attr("d", path)
-      // set the color of each county using pct_data map of county name to PCT_LACCESS_POP15
-      .attr("fill", function (d) {
-        return color(pct_data.get(d.properties.name));
-      })
-      .on("mouseover", mouseover_map) 
-      .on("mousemove", mousemove_map)
-      .on("mouseleave", mouseleave_map)
-      .on("click", county_clicked);
-  
-  map.append("g")
+    .attr("d", path)
+    // set the color of each county using pct_data map of county name to PCT_LACCESS_POP15
+    .attr("fill", function (d) {
+      return color(pct_data.get(d.properties.name));
+    })
+    .attr("id", function (d) {
+      const current_state = topojson_states.filter(function (s) {
+        return d.id.substring(0, 2) == s.id;
+      });
+      // set path's id = CountyState
+      return `${d.properties.name}${current_state[0].properties.name}`;
+    })
+    .on("mouseover", mouseover_map)
+    .on("mousemove", mousemove_map)
+    .on("mouseleave", mouseleave_map)
+    .on("click", county_clicked);
+
+  map
+    .append("g")
     .attr("id", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter()
     .append("path")
     .attr("d", path);
-    
+
   // add state borders
-  map.append("path")
+  map
+    .append("path")
     .datum(
       topojson.mesh(us, us.objects.states, function (a, b) {
         return a !== b;
@@ -130,7 +161,9 @@ function ready(us) {
 
 function county_clicked(event, d) {
   let county = d3.select(this);
-  if (county.classed("county-selected")) {
+  if (counties.length >= 3 && !county.classed("county-selected")) {
+    alert("Please select at most 3 counties");
+  } else if (county.classed("county-selected")) {
     county.classed("county-selected", false);
 
     // remove the county id from the list of counties to be included in the bar chart
@@ -138,16 +171,23 @@ function county_clicked(event, d) {
     var index = counties.indexOf(d.id);
     counties.splice(index, 1);
 
+    // update search bar selections
+    updateSearchBar();
+
     // update the bar chart
     update_bar();
     // update the scatter plot
     updateScatter();
-  }
-  else {
+  } else {
     county.classed("county-selected", true);
 
     // add the county id to the list of counties to be included in the bar chart
-    counties.push(d.id);
+    if (counties.indexOf(d.id) === -1) {
+      counties.push(d.id);
+    }
+
+    // update search bar selections
+    updateSearchBar();
 
     // update the bar chart
     update_bar();
