aa4f903af92335e5ffd582d80ac9c2a4e3118f17
ethanlee222 <lee.et@northeastern.edu>
2022-04-14 00:12:51 -0400

    finished bar chart (tooltip, axes, y scale, colors)

--- js/barchart-vis.js
+++ js/barchart-vis.js
@@ -45,8 +45,9 @@ function update_bar() {
       };
     });
 
-    // CLEAR ALL THE BARS
+    // CLEAR all the bars and axes
     svgBar.selectAll("rect").remove();
+    svgBar.selectAll('g').remove();
 
     // List of subgroups = header of the csv files = soil condition here
     const access = finalData.columns.slice(8);
@@ -54,8 +55,16 @@ function update_bar() {
     //stack the data? --> stack per subgroup
     const stackedData = d3.stack().keys(access)(data);
 
+    // get x domain
+    let domainX = data.map(function(d) {
+      if (counties.includes(d['FIPS'])) {
+        // return d.County;
+        return d.County + ', ' + d.State;
+      }
+    });
+
     // Add X axis
-    var x = d3.scaleBand().domain(counties).range([0, widthBar]).padding([0.2]);
+    var x = d3.scaleBand().domain(domainX).range([0, widthBar]).padding([0.2]);
     svgBar
       .append("g")
       .attr("transform", "translate(0," + heightBar + ")")
@@ -70,8 +79,15 @@ function update_bar() {
           .text("County")
       );
 
+    // Find max y
+    let maxY = d3.max(data, (d) => {
+      return Number(d.PCT_LACCESS_POP15) + 5;
+    });
+
+    let domainY = Math.min(maxY, 100);
+
     // Add Y axis
-    const y = d3.scaleLinear().domain([0, 50]).range([heightBar, 0]);
+    const y = d3.scaleLinear().domain([0, domainY]).range([heightBar, 0]);
     // .padding(0.2);
 
     svgBar
@@ -91,10 +107,10 @@ function update_bar() {
     svgBar
       .append("text")
       .attr("x", widthBar / 2)
-      .attr("y", marginBar.top - 50)
+      .attr("y", marginBar.top - 60)
       .attr("text-anchor", "middle")
       .style("font-size", "16px")
-      .text("County Low Access to Food");
+      .text("County Low Access to Grocery Stores");
 
     // Handmade legend
     svgBar
@@ -103,49 +119,49 @@ function update_bar() {
       .attr("y", -5)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#E67E22");
+      .style("fill", "#003f5c");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 15)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#F4D03F");
+      .style("fill", "#374c80");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 35)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#7D3C98");
+      .style("fill", "#7a5195");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 55)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#48C9B0");
+      .style("fill", "#bc5090");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 75)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#3498DB");
+      .style("fill", "#ef5675");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 95)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#34495E");
+      .style("fill", "#ff764a");
     svgBar
       .append("rect")
       .attr("x", 315)
       .attr("y", 115)
       .attr("width", 10)
       .attr("height", 10)
-      .style("fill", "#E74C3C");
+      .style("fill", "#ffa600");
     svgBar
       .append("text")
       .attr("x", 330)
@@ -201,14 +217,91 @@ function update_bar() {
       .scaleOrdinal()
       .domain(access)
       .range([
-        "#E74C3C",
-        "#34495E",
-        "#3498DB",
-        "#48C9B0",
-        "#7D3C98",
-        "#F4D03F",
-        "#E67E22",
+        "#ffa600",
+        "#ff764a",
+        "#ef5675",
+        "#bc5090",
+        "#7a5195",
+        "#374c80",
+        "#003f5c",
       ]);
+
+
+    // ----------------
+    // Create a tooltip
+    // ----------------
+
+    var bar_tooltip = d3.select("#barchart-vis")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .attr('id', "tooltip-map")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(event, d) {
+      const subgroupName = d3.select(this.parentNode).datum().key;
+      var subgroupValue = Number(d.data[subgroupName]).toFixed(2);
+
+      var MutableString = function(value) {
+        switch (value) {
+          case 'PCT_LACCESS_WHITE15':
+            this.text = 'White';
+            break;
+          case 'PCT_LACCESS_BLACK15':
+            this.text = 'Black';
+            break;
+          case 'PCT_LACCESS_HISP15':
+            this.text = 'Hispanic';
+            break;
+          case 'PCT_LACCESS_NHASIAN15':
+            this.text = 'Asian';
+            break;
+          case 'PCT_LACCESS_NHNA15':
+            this.text = 'American Indian';
+            break;
+          case 'PCT_LACCESS_NHPI15':
+            this.text = 'Hawaiian/Pacific';
+            break;
+          case 'PCT_LACCESS_MULTIR15':
+            this.text = 'Multiracial';
+            break;
+          default:
+            this.text = ''
+            console.log('default');
+        }
+      };
+
+      MutableString.prototype = {
+        toString: function() {
+          return this.text;
+        }
+      };
+
+      var demo_abbrev = new MutableString(subgroupName);
+
+      bar_tooltip
+          // .html("Demographic: " + demo_abbrev + "<br>" + "% Low Access: " + subgroupValue + "%")
+          .html(demo_abbrev + ": " + subgroupValue + "%")
+          .style("opacity", 1)
+    }
+
+    mousemove = function(event, d) {
+      bar_tooltip
+          .style("left", (event.pageX + 5)+"px")
+          .style("top", (event.pageY - 25) +"px");
+    }
+
+    var mouseleave = function(d) {
+      bar_tooltip
+          .style("opacity", 0)
+    }
+
 
     // Show the bars
     svgBar
@@ -228,20 +321,17 @@ function update_bar() {
       })
       .enter()
       .append("rect")
-      .attr("x", (d) => x(d.data["FIPS"]))
+      .attr("x", d => x(d.data["County"] + ", " + d.data["State"]))
       .attr("y", function (d) {
         return y(d[1]);
       })
       .attr("height", function (d) {
         return y(d[0]) - y(d[1]);
       })
-      .attr("width", x.bandwidth());
+      .attr("width", x.bandwidth())
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
 
-    legend = svgBar
-      .append("g")
-      .attr("class", "legend")
-      .attr("transform", "translate(50,30)")
-      .style("font-size", "12px")
-      .call(d3.legend);
   });
 }
